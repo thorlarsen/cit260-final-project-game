c464ecf4dc6eb71d52cc022f7323dc2f5a7d8576
Thor Larsen <thor.larsen@gmail.com>
2020-07-20 15:41:58 -0500

    Update Character.java

--- src/AdventueGame/Character.java
+++ src/AdventueGame/Character.java
@@ -1,31 +1,43 @@
 package AdventueGame;
 
 public abstract class Character {
+
     private int hitPoints;
     private int attackPoints;
+    private int roomNumber;
+    private String name;
+
 
     public Character(){
         /* default non-parameterized constructor
          * will create a default character object
          */
+        this(0, 0, 0, "dummy" );
+
     }
 
-    public Character(int hitPoints, int attackPoints){
+    public Character(int hitPoints, int attackPoints, int roomNumber, String name){
         /* default paramaterized object
          * this hitpoints = user input hitpoints
          * this attackpoints = user input attackpoints
          */
-    }
-
-    public int getAttackPoints() {
-        /* return character's attackpoints
-        */
+        this.hitPoints = hitPoints;
+        this.attackPoints = attackPoints;
+        this.roomNumber = roomNumber;
+        this.name = name;
     }
 
     public int getHitPoints() {
-        /* return character's hitpoints
-         */
+        return this.hitPoints;
     }
+
+    public int getAttackPoints() {
+        return this.attackPoints;
+    }
+
+    public int getRoomNumber() { return this.roomNumber; }
+
+    public String getName() { return this.name; }
 
     public void setHitPoints(int hitPoints) {
         this.hitPoints = hitPoints;
@@ -34,6 +46,10 @@ public abstract class Character {
     public void setAttackPoints(int attackPoints) {
         this.attackPoints = attackPoints;
     }
+
+    public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
+
+    public void setName(String name) { this.name = name; }
 
     public void attack(int getAttackPoints) {
         /*attack the opponent
